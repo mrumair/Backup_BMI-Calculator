ef942b41e88c66ffb786b16883b3b0a5e812191f
mrumair <33027331+mrumair@users.noreply.github.com>
2017-10-23 17:35:59 +0500

    Upload project files

---
+++ Driver/src/driver/Driver.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package driver;
+
+/**
+ *
+ * @author M Umair Bhatti
+ */
+public class Driver {
+
+    /**
+     * This class is for Testing of the class "BMI.java"
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        BMI bmi = new BMI();
+        bmi.show();
+    }
+ 
+}
